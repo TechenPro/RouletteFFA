3492d85a4b4e8fc62563826ed7bf8371c42a6072
Blue Peak High School <bpstu@tooeleschools.org>
2019-06-12 10:59:56 -0600

    First Commit

---
+++ src/bootstrap.js
@@ -0,0 +1,30 @@
+// Neccessary React Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Pulls the main component into the site.
+import App from './components/app';
+
+// Default styles of bootstrap.
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// Pulls main.scss into the site.
+import './style/main.scss';
+
+
+function main() {
+
+  ReactDOM.render(
+
+    /* 
+    Renders the main component <App/> into the app-wrapper <div>
+    Note how the `App` component is added like a custom HTML tag.
+    */
+
+    <App />
+    , document.querySelector('.app-wrapper'));
+    
+}
+
+// Runs the main() function once the ReactDOM has loaded.
+document.addEventListener('DOMContentLoaded', main);
---
+++ src/components/app.js
@@ -0,0 +1,43 @@
+// Imports the `React` module and the `Component` class from that module.
+import React, { Component } from 'react';
+
+// Imports the Header component from our header.js file. The ./ moves up one directory, in this case, out of this file.
+import Header from './header';
+
+// Creates an `App` class that is a child of the `Component` class.
+class App extends Component {
+
+  // The method that is called each time the class renders.
+  render() {
+    
+
+    // Sends the content that actually gets rendered by the component onto the site.
+    // ALL returned content MUST be nested inside a single tag.
+    return (
+
+      <div className='app'>
+
+        { /* 
+        Notice how you can use standard HTML tags, but this code comment is enclosed in curly brackets.
+        JSX allows you to use HTML and JS at the same time- all that has to be done is to wrap any JS code in {}.
+        */ }
+
+        { /*
+        Here we are rendering the Header component we imported above.
+        This is done by using a self closing custom tag with the name of the component we are rendering.
+        Just like any other HTML tage, we can pass in attributes- for example, the author attribute.
+        These parameters are accessed by the component as `props`.
+        We can also have multiple of the same component type- each different from each other in the rendering process.
+        */ }
+        <Header author='TechenPro'/>
+        <Header author='John Wayne'/>
+        <Header author='Harold'/>
+
+      </div>
+
+    );
+  }
+}
+
+// Exports the `App` component so it can be rendered.
+export default App;
